0a13fcb7e1f39c66909a5453e5a5fa3db65f62ec
armand <armand.lecomte@epitech.eu>
2026-04-27 16:39:44 +0200

    Fix(LightCinfig): fixed ifndef name and includes in Lightconfig.hpp

--- include/Objects/Lights/LightConfig.hpp
+++ include/Objects/Lights/LightConfig.hpp
@@ -5,12 +5,12 @@
 ** ${descriptor}
 */
 
-#ifndef $LIGHTCONFIG_HPP
-#define $LIGHTCONFIG_HPP
-#include <memory>
-#include <vector>
+#ifndef LIGHTCONFIG_HPP
+    #define LIGHTCONFIG_HPP
+    #include <memory>
+    #include <vector>
 
-#include "ILightSource.hpp"
+    #include "ILightSource.hpp"
 
 namespace RayTracer {
     class LightConfig {
@@ -29,4 +29,4 @@ namespace RayTracer {
     };
 }
 
-#endif //$LIGHTCONFIG_HPP
+#endif //LIGHTCONFIG_HPP
