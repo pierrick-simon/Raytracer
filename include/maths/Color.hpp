b4d6224d6bd35d0e6a516d300338d4eddc438797
Natan Pereira <natan.pereira@epitech.eu>
2026-05-09 17:19:17 +0200

    feat(Color): Color class that stores as double instead of RGB

--- include/maths/Color.hpp
+++ include/maths/Color.hpp
@@ -10,12 +10,35 @@
     #include "Vector.hpp"
 
 namespace Maths {
+    class Color : public Vector<4, double> {
+    public:
+        Color() = default;
 
-class Color : Vector<4, double> {
-public:
-    static Color fromRGB(unsigned char);
-};
+        Color(double r, double g, double b, double a = 1.0);
 
+        explicit Color(const Vector3D &color, double a = 1.0);
+
+        static Color from8Bit(unsigned char r, unsigned char g, unsigned char b,
+            unsigned char a =  std::numeric_limits<unsigned char>::max());
+
+        [[nodiscard]] Vector<4, unsigned char> to8Bit() const;
+
+        [[nodiscard]] double &getR();
+
+        [[nodiscard]] const double &getR() const;
+
+        [[nodiscard]] double &getG();
+
+        [[nodiscard]] const double &getG() const;
+
+        [[nodiscard]] double &getB();
+
+        [[nodiscard]] const double &getB() const;
+
+        [[nodiscard]] double &getA();
+
+        [[nodiscard]] const double &getA() const;
+    };
 } // Maths
 
 #endif
--- src/maths/Color.cpp
+++ src/maths/Color.cpp
@@ -7,5 +7,81 @@
 
 #include "Color.hpp"
 
+#include <algorithm>
+#include <cmath>
+
 namespace Maths {
+    Color::Color(double r, double g, double b, double a) :
+        Vector(r, g, b, a)
+    {
+    }
+
+    Color::Color(const Vector3D &color, double a) :
+        Vector(color.getX(), color.getY(), color.getZ(), a)
+    {
+    }
+
+    Color Color::from8Bit(unsigned char r, unsigned char g, unsigned char b,
+        unsigned char a)
+    {
+        return {
+            r / 255.0,
+            g / 255.0,
+            b / 255.0,
+            a / 255.0
+        };
+    }
+
+    static unsigned char doubleToByte(double val)
+    {
+        val = std::clamp(val, 0.0, 1.0);
+        return static_cast<unsigned char>(std::lround(val * 255.0));
+    }
+
+    Vector<4, unsigned char> Color::to8Bit() const
+    {
+        return Vector<4, unsigned char>(doubleToByte(getR()),
+            doubleToByte(getG()),
+            doubleToByte(getB()), doubleToByte(getA()));
+    }
+
+    double &Color::getR()
+    {
+        return getX();
+    }
+
+    const double &Color::getR() const
+    {
+        return getX();
+    }
+
+    double &Color::getG()
+    {
+        return getY();
+    }
+
+    const double &Color::getG() const
+    {
+        return getY();
+    }
+
+    double &Color::getB()
+    {
+        return getZ();
+    }
+
+    const double &Color::getB() const
+    {
+        return getZ();
+    }
+
+    double &Color::getA()
+    {
+        return getW();
+    }
+
+    const double &Color::getA() const
+    {
+        return getW();
+    }
 } // Maths
