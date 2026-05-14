555cd229a551fe67eac788d9fb22b00dd19de58e
armand <armand.lecomte@epitech.eu>
2026-05-14 14:19:17 +0200

    Feat(Cones): added non infinite cones hits method

--- plugins/primitive/raytracer_cone/include/PrimitiveCone.hpp
+++ plugins/primitive/raytracer_cone/include/PrimitiveCone.hpp
@@ -17,8 +17,22 @@ namespace RayTracer {
         PrimitiveCone(const Maths::Point3D &origin, double radius,
             std::optional<double> height, Material const &Material);
 
+        std::optional<HitInfo> hitsInfinite(Ray const & ray);
+
+        void getBottomCapBestT(std::optional<double> &bestT, double t,
+            double hx,
+            double hy) const;
+
+        bool getSlideBestT(std::optional<double> &bestT, double t, double hitZ);
+
+        std::optional<HitInfo> hitsCone(Ray const & ray);
+
         std::optional<HitInfo> hits(const Ray &ray) override;
-        HitInfo fillHitInfo(const Ray &ray, double t) const;
+
+        [[nodiscard]] Maths::Vector3D coneNormal(Maths::Point3D point3) const;
+        std::optional<double> hitSlide(Ray const &ray, Maths::Vector3D diff, double a, double b, double delta);
+        void hitCap(Ray const &ray, Maths::Vector3D diff, std::optional<double> bestT) const;
+        [[nodiscard]] HitInfo fillHitInfo(const Ray &ray, double t) const;
 
         [[nodiscard]] const Maths::Point3D &getOrigin() const;
         [[nodiscard]] Maths::Point3D &getOrigin();
--- plugins/primitive/raytracer_cone/src/PrimitiveCone.cpp
+++ plugins/primitive/raytracer_cone/src/PrimitiveCone.cpp
@@ -5,7 +5,6 @@
 ** ${descriptor}
 */
 
-
 #include "RayTracer.hpp"
 #include "PrimitiveCone.hpp"
 
@@ -17,16 +16,111 @@ namespace RayTracer {
     {
     }
 
+    std::optional<HitInfo> PrimitiveCone::hitsInfinite(Ray const &ray)
+    {
+        return std::nullopt;
+    }
+
+    void PrimitiveCone::getBottomCapBestT(std::optional<double> &bestT,
+        double t, double hx, double hy) const
+    {
+        if (hx * hx + hy * hy <= _radius * _radius && (!bestT.has_value()
+            || t < bestT.value()))
+            bestT = t;
+    }
+
+    bool PrimitiveCone::getSlideBestT(std::optional<double> &bestT, double t,
+        double hitZ)
+    {
+        if (hitZ >= 0.0 && hitZ <= _height.value()) {
+            if (!bestT.has_value() || t < bestT.value())
+                bestT = t;
+            return true;
+        }
+        return false;
+    }
+
+    std::optional<double> PrimitiveCone::hitSlide(Ray const &ray, Maths::Vector3D diff,
+        double a, double b, double delta)
+    {
+        std::optional<double> bestT = std::nullopt;
+        double sqrtDelta = sqrt(delta);
+        for (double const t: {(-b - sqrtDelta) / (2.0 * a),
+            (-b + sqrtDelta) / (2.0 * a)}) {
+            if (t < DOUBLE_OFFSET)
+                continue;
+            double hitZ = diff.getZ() + ray.direction.getZ() * t;
+            if (getSlideBestT(bestT, t, hitZ))
+                break;
+        }
+        return bestT;
+    }
+
+    void PrimitiveCone::hitCap(Ray const &ray, Maths::Vector3D diff,
+        std::optional<double> bestT) const
+    {
+        if (std::abs(ray.direction.getZ()) > DOUBLE_OFFSET) {
+            double t = -diff.getZ() / ray.direction.getZ();
+            if (t > DOUBLE_OFFSET) {
+                double hx = diff.getX() + ray.direction.getX() * t;
+                double hy = diff.getY() + ray.direction.getY() * t;
+                getBottomCapBestT(bestT, t, hx, hy);
+            }
+        }
+    }
+
+    std::optional<HitInfo> PrimitiveCone::hitsCone(Ray const &ray)
+    {
+        Maths::Vector3D diff = ray.origin - _origin;
+        std::optional<HitInfo> hitInfo = std::nullopt;
+        double k = (_radius / _height.value()) * (_radius / _height.value());
+        double a = ray.direction.getX() * ray.direction.getX() + ray.direction.
+            getY() * ray.direction.getY() - k * ray.direction.getZ() * ray.
+            direction.getZ();
+        double b = 2.0 * (diff.getX() * ray.direction.getX() + diff.getY() * ray.direction.getY()
+            - k * (diff.getZ() - _height.value()) * ray.direction.getZ());
+        double c = diff.getX() * diff.getX() + diff.getY() * diff.getY() - k * (
+            diff.getZ() - _height.value()) * (diff.getZ() - _height.value());
+        double delta = b * b - 4.0 * a * c;
+
+        if (delta < 0.0)
+            return std::nullopt;
+        std::optional<double> bestT = hitSlide(ray, diff, a, b, delta);
+        hitCap(ray, diff, bestT);
+        if (bestT.has_value())
+            hitInfo = fillHitInfo(ray, bestT.value());
+        return hitInfo;
+    }
+
     std::optional<HitInfo> PrimitiveCone::hits(const Ray &ray)
     {
-        // TODO
-        return std::nullopt;
+        return _height == std::nullopt ? hitsInfinite(ray) : hitsCone(ray);
+    }
+
+    Maths::Vector3D PrimitiveCone::coneNormal(Maths::Point3D hitPos) const
+    {
+        double pz = hitPos.getZ() - _origin.getZ();
+
+        if (pz <= DOUBLE_OFFSET)
+            return Maths::Vector3D{0, 0, -1};
+        double nx = hitPos.getX() - _origin.getX();
+        double ny = hitPos.getY() - _origin.getY();
+        double len = sqrt(nx * nx + ny * ny);
+        double slide = _radius / _height.value();
+
+        Maths::Vector3D normal{nx / len * slide, ny / len * slide, slide};
+        return normal.normalized();
     }
 
     HitInfo PrimitiveCone::fillHitInfo(const Ray &ray, double t) const
     {
         HitInfo hit;
-        // TODO
+        hit.hitPos.getX() = ray.origin.getX() + ray.direction.getX() * t;
+        hit.hitPos.getY() = ray.origin.getY() + ray.direction.getY() * t;
+        hit.hitPos.getZ() = ray.origin.getZ() + ray.direction.getZ() * t;
+        hit.hitDist = hit.hitPos.distance(ray.origin);
+        hit.impactNormal = coneNormal(hit.hitPos);
+        hit.material = _material;
         return hit;
     }
 
